3e5af589603e039900d8b11b9e4bf88003e83f89
Gabriel de Morais <gabrieldemorais6@gmail.com>
2022-11-26 23:20:30 -0300

    feat: add interaction between player and grass collision boxes

--- World/Grass/Grass.cs
+++ World/Grass/Grass.cs
@@ -2,19 +2,22 @@ using Godot;
 
 public class Grass : Node2D
 {
-	public override void _Process(float delta)
+	public void _OnHurtboxAreaEntered(Area area)
 	{
-		if (Input.IsActionJustPressed("attack"))
-		{
-			var scene = GD.Load<PackedScene>("res://Effects/GrassEffect.tscn");
-			var instance = scene.Instance<GrassEffect>();
-			instance.GlobalPosition = GlobalPosition;
+		CreateGrassEffect();
+		QueueFree();
+	}
 
-			var world = GetTree().CurrentScene;
-			world.AddChild(instance);
+	private void CreateGrassEffect()
+	{
+		var scene = GD.Load<PackedScene>("res://Effects/GrassEffect.tscn");
+		var instance = scene.Instance<GrassEffect>();
+		instance.GlobalPosition = GlobalPosition;
 
-			// Deletes this grass' instance after the current frame's execution
-			QueueFree();
-		}
+		var world = GetTree().CurrentScene;
+		world.AddChild(instance);
+
+		// Deletes this grass' instance after the current frame's execution
+		QueueFree();
 	}
 }

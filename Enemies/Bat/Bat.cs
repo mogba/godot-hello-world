c66a99679a93791b80da008127fbd897e3c506bb
Gabriel de Morais <gabrieldemorais6@gmail.com>
2022-11-27 21:57:37 -0300

    feat(enemy bat): start basic chase player logic

--- Enemies/Bat/Bat.cs
+++ Enemies/Bat/Bat.cs
@@ -5,15 +5,31 @@ public class Bat : KinematicBody2D
 	private static PackedScene _enemyDeathEffectScene =
 		ResourceLoader.Load<PackedScene>("res://Effects/EnemyDeathEffect/EnemyDeathEffect.tscn");
 
+	private Vector2 _velocity = Vector2.Zero;
 	private Vector2 _knockback = Vector2.Zero;
-	private int _knockbackForce = 200;
+	private AnimationStateType _animationState = AnimationStateType.CHASE;
+
 	private AnimatedSprite _animatedSprite = null;
 	private Status _status = null;
+	private PlayerDetectionZone _playerDetectionZone = null;
+
+	[Export] public int KnockbackForce = 200;
+	[Export] public int Acceleration = 300;
+	[Export] public int Friction = 200;
+	[Export] public int MaxSpeed = 50;
+
+	private enum AnimationStateType
+	{
+		IDLE,
+		WANDER,
+		CHASE
+	}
 
 	public override void _Ready()
 	{
 		_animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
 		_status = GetNode<Status>("Status");
+		_playerDetectionZone = GetNode<PlayerDetectionZone>("PlayerDetectionZone");
 
 		_animatedSprite.Frame = 0;
 		_animatedSprite.Play("Animate");
@@ -21,8 +37,33 @@ public class Bat : KinematicBody2D
 
 	public override void _PhysicsProcess(float delta)
 	{
-		_knockback = _knockback.MoveToward(Vector2.Zero, _knockbackForce * delta);
+		_knockback = _knockback.MoveToward(Vector2.Zero, KnockbackForce * delta);
 		_knockback = MoveAndSlide(_knockback);
+
+		switch (_animationState)
+		{
+			case AnimationStateType.IDLE:
+				_velocity = _velocity.MoveToward(Vector2.Zero, Friction * delta);
+				SeekAndChasePlayer();
+				break;
+			case AnimationStateType.WANDER:
+				break;
+			case AnimationStateType.CHASE:
+				if (_playerDetectionZone.IsPlayerDetected())
+				{
+					var newPosition = (_playerDetectionZone.Player.GlobalPosition - GlobalPosition).Normalized();
+					_velocity = _velocity.MoveToward(newPosition * MaxSpeed, Acceleration * delta);
+
+					_animatedSprite.FlipH = _velocity.x < 0;
+				}
+				else
+				{
+					_animationState = AnimationStateType.IDLE;
+				}
+				break;
+		}
+
+		_velocity = MoveAndSlide(_velocity);
 	}
 
 	public void _OnHurtboxAreaEntered(Area2D area)
@@ -38,5 +79,14 @@ public class Bat : KinematicBody2D
 		QueueFree();
 	}
 
-	private void CreateEnemyDeathEffect() => _enemyDeathEffectScene.Instance<Effect>().AttachToRoot(this);
+	private void CreateEnemyDeathEffect()
+		=> _enemyDeathEffectScene.Instance<Effect>().AttachToRoot(this);
+
+	private void SeekAndChasePlayer()
+	{
+		if (_playerDetectionZone.IsPlayerDetected())
+		{
+			_animationState = AnimationStateType.CHASE;
+		}
+	}
 }
